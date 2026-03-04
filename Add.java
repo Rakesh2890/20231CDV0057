7d4d8d7799c02a319d0af2a10cd60cafdf665c26
Rakesh2890 <rakesh.raki.3678@gmail.com>
2026-03-04 05:45:33 +0000

    final commit

---
+++ Add.java
@@ -0,0 +1,8 @@
+public class Add {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+}
